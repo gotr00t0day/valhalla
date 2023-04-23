5a014ae125ca43f3551a86d0be2ff423f9ce449a
Miguel Sanchez <c0deninja@Luisa-D.local>
2023-04-22 21:47:57 -0400

    added vuln scanning with nuclei

--- valhalla.py
+++ valhalla.py
@@ -1,9 +1,7 @@
-from modules import sub_output, fetch_requests
+from modules import fetch_requests
 from colorama import Fore
 import argparse
 import shodan
-import requests
-import urllib3
 
 banner = f"""
 
@@ -27,7 +25,7 @@ ____   ____      .__  .__           .__  .__
    \___/  (____  /____/___|  (____  /____/____(____  /
                \/          \/     \/               \/ 
                Author:  c0deninja
-               Version: v1.3
+               Version: v1.4
 
 
 """
@@ -51,15 +49,21 @@ parser.add_argument('-f', '--file',
 parser.add_argument('-cve', '--cve_id',
                    help="scan by cve id")
 
-parser.add_argument('-vuln', '--vulnerability',
+parser.add_argument('-vuln', '--vulnerability', action='store_true',
                    help="scan for vulnerabilities")
+
+parser.add_argument('-ips', '--ipaddresses', action='store_true',
+                   help="getting ip addresses from shodan")
 
 args = parser.parse_args()
 
 print(banner)
 
-SHODAN_API_KEY = ""
-api = shodan.Shodan(SHODAN_API_KEY)
+if args.dork:
+    if args.ipaddresses:
+        print(f"Extracting IP Adddresses from {Fore.CYAN}Shodan{Fore.RESET}...")
+        shodan_con.ips(args.dork)
+        print(f"DONE{Fore.MAGENTA}!\n")
 
 if args.target:
     if args.port:
@@ -68,39 +72,64 @@ if args.target:
 
 if args.target:
     if args.port:
-        fetch_requests.scan(args.target,args.port)
+        fetch_requests.scan(args.target, args.port)
 
 if args.file:
     if args.cve_id:
         with open(f'{args.file}', 'r') as f:
             domain_list = [x.strip() for x in f.readlines()]
             for domains in domain_list:
-                fetch_requests.cve_scan_file(args.file,args.cve_id) 
+                fetch_requests.cve_scan_file(args.file, args.cve_id) 
 
 if args.file:
  if args.port:
     with open(f'{args.file}', 'r') as f:
         domain_list = [x.strip() for x in f.readlines()]
         for domains in domain_list:
-            fetch_requests.scan(domains,args.port)
+            fetch_requests.scan(domains, args.port)
 
 if args.dork:
     if args.port:
         if args.cve_id:
             try:
-                results = api.search(str(args.dork))
-                ips = []
-                for result in results['matches']:
-                    ips.append(result['ip_str'])
-                with open("ips.txt", "w") as f:
-                    for ipaddresses in ips:
-                        f.writelines(f"{ipaddresses}\n")
+                shodan_con.ips(args.dork)
                 with open('ips.txt', 'r') as f2:
                     ip_list = [x.strip() for x in f2.readlines()]
                     for iplist in ip_list:
-                        fetch_requests.cve_scan_dork(iplist,args.port,args.cve_id)                  
+                        fetch_requests.cve_scan_dork(iplist, args.port, args.cve_id)                  
             except shodan.APIError as e:
                 print(e)
         else:
             pass
-    
+
+if args.dork:
+    if args.port:
+        try:
+            shodan_con.ips(args.dork)
+            with open('ips.txt', 'r') as f2:
+                ip_list = [x.strip() for x in f2.readlines()]
+                for iplist in ip_list:
+                    fetch_requests.scan(iplist, args.port)                  
+        except shodan.APIError as e:
+            print(e)
+    else:
+        pass
+
+if args.target:
+    if args.vulnerability:
+        if args.port:
+            fetch_requests.vuln_scan(args.vulnerability, args.port)
+
+if args.dork:
+    if args.port:
+        if args.vulnerability:
+            try:
+                shodan_con.ips(args.dork)
+                with open('ips.txt', 'r') as f2:
+                    ip_list = [x.strip() for x in f2.readlines()]
+                    for iplist in ip_list:
+                        fetch_requests.vuln_dork(iplist, args.port, args.vulnerability)                  
+            except shodan.APIError as e:
+                print(e)
+        else:
+            pass
