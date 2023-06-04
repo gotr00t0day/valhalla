8e4008bf20eca157425687a4926f2fea5f3e7b77
Miguel Sanchez <c0deninja@Luisa-D.local>
2023-06-04 09:27:59 -0400

    nuclei and nuclei-templates installation

---
+++ install.sh
@@ -0,0 +1,8 @@
+# Install nuclei
+
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+
+# install nuclei templates
+
+cd ~ 
+git clone https://github.com/projectdiscovery/nuclei-templates.git
