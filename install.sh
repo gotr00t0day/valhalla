391e1d59b0f0181e0fe3807b676d4ad7f210b507
Miguel Sanchez <c0deninja@Luisa-D.local>
2023-06-04 20:10:15 -0400

    updates to installation

--- install.sh
+++ install.sh
@@ -1,8 +1,26 @@
-# Install nuclei
+#!/bin/bash
 
-go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+# Check if Go is installed
+if ! command -v go &> /dev/null; then
+    echo "Go is not installed. Installing..."
+    # Install Go
+    # Add your installation commands here
+fi
 
-# install nuclei templates
+# Check if Nuclei is installed
+if ! command -v nuclei &> /dev/null; then
+    echo "Nuclei is not installed. Installing..."
+    # Install Nuclei
+    # Add your installation commands here
+fi
 
-cd ~ 
-git clone https://github.com/projectdiscovery/nuclei-templates.git
+# Change directory to root
+cd /
+
+# Clone Nuclei templates repository
+if [ ! -d nuclei-templates ]; then
+    echo "Cloning Nuclei templates repository..."
+    git clone https://github.com/projectdiscovery/nuclei-templates.git
+else
+    echo "Nuclei templates repository already exists."
+fi
